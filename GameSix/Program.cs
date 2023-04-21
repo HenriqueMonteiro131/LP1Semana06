e5a9f1ec122dd6b087b0480831a0f199b7e74e81
HenriqueMonteiro <Henriquepupet@gmail.com>
2023-04-21 12:29:35 +0100

    Fixed the Program code, added writline for the names

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -7,18 +7,23 @@ namespace GameSix
         static void Main(string[] args)
         {
  
-            Console.Console.WriteLine("Hi there traveler! Please tell me"+ 
+            Console.WriteLine("Hi there traveler! Please tell me"+ 
             "the number of enemies between 1 and 9 for this simulation:");
             int number = int.Parse(Console.ReadLine());
-            string[] names = new string[n];
+            string[] names = new string[number];
 
-            for (int i = 0; i < number; i++)
-{
-            Console.Write("Enter a name for an enemy " + i + ": ");
-            names[i] = Console.ReadLine();
-}
+                for (int i = 0; i < number; i++)
+                
+                    {
+                        Console.WriteLine("Enter a name for an enemy " + i + ": ");
+                            names[i] = Console.ReadLine();
+                    
+                    }
 
-
+                foreach (string n in names)
+                {
+                    Console.WriteLine(n);
+                } 
 
             }
      }    
